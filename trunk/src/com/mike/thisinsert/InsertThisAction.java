3f03609c27301797546fdb9465313606f4495306
mjparme <mjparme@0a751614-0846-11de-bc6d-132d5dcf21d4>
2009-03-03 23:00:04 +0000

    initial import

---
+++ trunk/src/com/mike/thisinsert/InsertThisAction.java
@@ -0,0 +1,22 @@
+package com.mike.thisinsert;
+
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+
+/**
+ * User: mjparmel
+ * Date: Feb 27, 2009
+ * Time: 2:13:28 PM
+ */
+public class InsertThisAction extends AnAction {
+    private static final Logger logger = Logger.getInstance(InsertThisAction.class.getName());
+
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+        ThisInserter inserter = new ThisInserter(event.getDataContext(), project);
+        WriteActionRunner runner = new WriteActionRunner(inserter);
+        CommandProcessor.getInstance().executeCommand(project, runner, "Insert This", null);
+    }
+}
